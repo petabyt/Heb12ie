24476553c92663c73b5c7196f9916500b3c5632e
Petabyte Studios <brikbusters@gmail.com>
2019-04-06 11:16:09 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var books = [ "Genesis", "Exodus", "Leviticus", "Numbers", "Deuteronomy", "Joshua", "Judges", "Ruth", "1st Samuel", "2nd Samuel", "1st Kings", "2nd Kings", "1st Chronicles", "2nd Chronicles", "Ezra", "Nehemiah", "Esther", "Job", "Psalms", "Proverbs", "Ecclesiastes", "Song of Solomon", "Isaiah", "Jeremiah", "Lamentations", "Ezekiel", "Daniel", "Hosea", "Joel", "Amos", "Obadiah", "Jonah", "Micah", "Nahum", "Habakkuk", "Zephaniah", "Haggai", "Zechariah", "Malachi", "Matthew", "Mark", "Luke", "John", "Acts", "Romans", "1st Corinthians", "2nd Corinthians", "Galatians", "Ephesians", "Philippians", "Colossians", "1st Thessalonians", "2nd Thessalonians", "1st Timothy", "2nd Timothy", "Titus", "Philemon", "Hebrews", "James", "1st Peter", "2nd Peter", "1st John", "2nd John", "3rd John", "Jude", "Revelation" ];
+var bookNum = 0;
 function update() {
 	var book = document.getElementById('book').value;
 	var chapter = Number(document.getElementById('chapter').value) - 1;
 
-	var bookNum = 0;
 	for (var i = 0; i < books.length; i++) {
 		if (books[i] == book) {
 			bookNum = i;
@@ -21,4 +21,24 @@ function update() {
 window.onload = function() {
 	document.getElementById('loading').style.display = "none";
 	update();
+}
+
+function next() {
+	var chapter = Number(document.getElementById('chapter').value);
+	document.getElementById('chapter').options[chapter].selected = true
+	update();
+}
+
+function previous() {
+	var chapter = Number(document.getElementById('chapter').value);
+	document.getElementById('chapter').options[chapter - 2].selected = true
+	update();
+}
+
+function updateChapter() {
+	var numbers = "";
+	for (var i = 0; i < bbe[bookNum].chapters.length; i++) {
+		numbers += "<option>" + (i + 1) + "</option>";
+	}
+	document.getElementById('chapter').innerHTML = numbers;
 }
